f6e9460c47f4c2cea60dfc4f82aa469b94a8d027
Anton <anton.stepanov97@mail.ru>
2022-12-30 23:28:26 +0600

    Задача 24

--- Семинар 4/1/Program.cs
+++ Семинар 4/1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.Write((1 + n) / 2 * n);
+
+
