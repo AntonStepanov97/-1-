391d7b368fb50debd6ad9a0ac98917b1e44a30d3
Anton <anton.stepanov97@mail.ru>
2022-12-30 21:11:58 +0600

    add Семинар 1, папка 3

--- Семинар 3/2/Program.cs
+++ Семинар 3/2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear;
+Console.Write("Введите координату x первой точки: ");
+double x_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y первой точки: ");
+double y_2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x второй точки: ");
+double x_1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y второй точки: ");
+double y_2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x_1-x_2))+(Math.Pow(y_1-y_2)));
+
