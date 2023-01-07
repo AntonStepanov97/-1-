d410a3e34e18a3c1fe6255480a17fafec26ea0a9
Anton <anton.stepanov97@mail.ru>
2023-01-07 17:34:22 +0600

    add Поиск равного

--- Лекции/Гирьки/Program.cs
+++ Лекции/Гирьки/Program.cs
@@ -1,15 +1,14 @@
-﻿int a = 1;
-int b = 2;
-int c = 6;
-int d = 8;
-int e = 4;
+﻿Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
 
-int max = a;
+int[] array = {18,23,34,45,52,64,71,85,92,};
+array[0] = 12;
+Console.WriteLine(array[0]);
 
-if(a > max) max = a;
-if(b > max) max = b;
-if(c > max) max = c;
-if(d > max) max = d;
-if(e > max) max = e;
 
-Console.WriteLine(max);
