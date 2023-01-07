85c03a64c0bdcb3c64016526e9d06c14b238f945
Anton <anton.stepanov97@mail.ru>
2023-01-08 00:12:16 +0600

    add text redactor1

---
+++ Лекции/arrange array/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+int[] array = {1, 5, 6, 2, 3, 7, 4, 1, 4};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void arrange(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i ++)
+    {
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j; // для изменения порядка изменить знак //
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(array);
+arrange(array);
+PrintArray(array);
