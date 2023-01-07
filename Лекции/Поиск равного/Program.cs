3c970cfe8264ec28f449463f14ba3c6e3dc758e0
Anton <anton.stepanov97@mail.ru>
2023-01-07 22:13:10 +0600

    add Синтаксис

--- Лекции/Поиск равного/Program.cs
+++ Лекции/Поиск равного/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int[] array = {1,2,6,7,4,89,686};
+
+int n = array.Length;
+int find = 89;
+
+int index = 0;
+
+while (index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
