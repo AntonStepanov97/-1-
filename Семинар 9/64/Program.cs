e6c9a9ad0ab84e827b3b2df5cd4465842a1a5b5c
Anton <anton.stepanov97@mail.ru>
2023-02-11 15:11:40 +0600

    add task 64

---
+++ Семинар 9/64/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int n = InputInt("Введите большее положительное число ");
+int m = InputInt("Введите меньшее положительное число ");
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
