b13ed12290296a6b0275062445287578f01af031
Anton <anton.stepanov97@mail.ru>
2023-02-11 15:12:26 +0600

    add task 64

--- Лекции/string_двумерные массивы/Program.cs
+++ Лекции/string_двумерные массивы/Program.cs
@@ -27,7 +27,7 @@ for (int i = 0; i < matr.GetLength(0); i++)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(1); i++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
              matr[i,j] = new Random().Next(1,10);
         }
